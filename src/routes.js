4012c4f98210681d52eb0b313d91042741fc5add
miqueiasbelfort <kawasousa1554@gmail.com>
2022-07-18 17:47:48 -0300

    new icon

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,10 @@ import Notification from "./pages/Notifications"
 import Profile from "./pages/Profile"
 import Search from "./pages/Search"
 
+import ButtonNew from "./components/buttonNew"
+
+import {Entypo, Feather} from "@expo/vector-icons"
+
 const Tab = createBottomTabNavigator()
 
 export default function Routes() {
@@ -18,13 +22,58 @@ export default function Routes() {
                 borderTopColor: "trasparent"
             },
             tabBarActiveTintColor: "#fff",
+            tabBarItemStyle: {
+                paddingBottom: 5,
+                paddingTop: 5
+            }
            }}
         >
-            <Tab.Screen name="Home" component={Home}/>
-            <Tab.Screen name="Search" component={Search}/>
-            <Tab.Screen name="New" component={New}/>
-            <Tab.Screen name="Notification" component={Notification}/>
-            <Tab.Screen name="Profile" component={Profile}/>
+            <Tab.Screen 
+                name="Home" 
+                component={Home}
+                options={{
+                    tabBarIcon: ({size, color}) => (
+                        <Entypo name="home" size={size} color={color}/>
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="Search" 
+                component={Search}
+                options={{
+                    tabBarIcon: ({ size, color}) => (
+                        <Feather name="search" size={size} color={color}/>
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="New" 
+                component={New}
+                options={{
+                    tabBarLabel: "",
+                    tabBarIcon: ({size, color, focused}) => (
+                        <ButtonNew size={size} color={color} focused={focused}/>
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="Notification" 
+                component={Notification}
+                options={{
+                    tabBarIcon: ({size, color}) => (
+                        <Entypo name="notification" size={size} color={color}/>
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="Profile" 
+                component={Profile}
+                options={{
+                    tabBarIcon: ({size, color}) => (
+                        <Feather name="user" size={size} color={color}/>
+                    )
+                }}
+            />
         </Tab.Navigator>
     )
 }
